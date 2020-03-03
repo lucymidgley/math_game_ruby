a4165f50ad42b01977e97890e704ed8cda198b5d
Lucy <lucymidgley13@gmail.com>
2020-03-03 00:54:38 +0000

    added coloured text to make game commands easier to see

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require "./game.rb"
 require "./players.rb"
+require 'colorize'
 
 def play_game
-  puts "please enter the name for player 1"
+  puts "please enter the name for player 1".green
   name_player1 = gets.chomp
-  puts "please enter the name for player 2"
+  puts "please enter the name for player 2".green
   name_player2 = gets.chomp
   puts "Lets play!"
   player1 = Player.new(name_player1)
@@ -18,24 +19,24 @@ def play_game
     val2 = game.gen_num
     q_and_a = game.ask_question(val1, val2)
     if n % 2 == 0 
-    puts "----- New Round! -----" 
+    puts "----- New Round! -----".blue 
     end
     puts "Player #{player.name} #{q_and_a[0]}"
     solution = gets.chomp
     if solution.to_i == q_and_a[1] 
       player.add_point 
-      puts "CORRECT!"
+      puts "CORRECT!".green
      else
-      puts "INCORRECT!"
+      puts "INCORRECT!".red
      end
-    puts "--- Current score: player #{player1.name} #{player1.score} / 3 vs  player #{player2.name} #{player2.score} / 3 ---"
+    puts "--- Current score: player #{player1.name} #{player1.score} / 3 vs  player #{player2.name} #{player2.score} / 3 ---".yellow
   }
-  puts "==== GAME OVER!! ===="
+  puts "==== GAME OVER!! ====".red
 if player1.score == player2.score 
-    puts "These players are evenly matched! Player #{player1.name} #{player1.score}/3 and Player #{player2.name} also #{player2.score}/3!" 
+    puts "These players are evenly matched! Player #{player1.name} #{player1.score}/3 and Player #{player2.name} also #{player2.score}/3!".bold.blue 
 else 
   winner = player1.score > player2.score ? player1 : player2
-  puts "The winner is #{winner.name} with a score of #{winner.score}/3 !"
+  puts "The winner is #{winner.name} with a score of #{winner.score}/3 !".bold.blue
 end
 end
 
